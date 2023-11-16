a3501ee2a9bf4a0d3d115b608362cdf083720cb9
ynwsaurabh <ynwsaurabh@gmail.com>
2023-11-16 17:39:11 +0530

    Task 2

---
+++ app/src/main/java/com/example/to_dolist/DialogCloseListener.java
@@ -0,0 +1,7 @@
+package com.example.to_dolist;
+
+import android.content.DialogInterface;
+
+public interface DialogCloseListener {
+    public void handleDialogClose(DialogInterface dialog);
+}
---
+++ app/src/main/java/com/example/to_dolist/MainActivity1.java
@@ -0,0 +1,7 @@
+package com.example.to_dolist;
+
+import android.content.DialogInterface;
+
+public interface MainActivity1 {
+    void handleDialogClose(DialogInterface dialog);
+}
